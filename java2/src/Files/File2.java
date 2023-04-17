8338ec245cb571ec4f6a9a9bbdbf7d95efbf4fc0
s0415 <sheepmeow1107@gmail.com>
2023-04-17 15:02:40 +0900

    master

--- java2/src/Files/File2.java
+++ java2/src/Files/File2.java
@@ -5,10 +5,9 @@ public class File2 {
 
 	public static void main(String[] args) throws Exception {
 		try {
-		FileReader fr = new FileReader("D:\\git_java\\javaproject\\java2\\src\\Files\\message.txt");
+		FileReader fr = new FileReader("D:\\git_java\\javaproject\\java2\\src\\Files\\agree.txt");
 		System.out.println(fr.getEncoding());	//파일 인코딩 언어 확인
 		System.out.println(fr.read());	//데이터 크기
-		System.out.println(fr.read());
 		fr.close();//로드한 파일을 닫을때 사용
 		}
 		catch (Exception e){
