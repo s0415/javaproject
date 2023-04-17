c2725cddb5f97d45fd921e57adcc3e6f67c73565
s0415 <sheepmeow1107@gmail.com>
2023-04-17 14:43:01 +0900

    master

--- java2/src/Files/File1.java
+++ java2/src/Files/File1.java
@@ -1,8 +1,22 @@
 package Files;
 
-public class File1 {
+import java.io.FileReader;
+import java.io.IOException;
 
-	public static void main(String[] args) {
+public class File1 {
+	// I/O : InputStream, OutputStream
+		/*
+		 입력 스트림 : 프로그램으로 들어오는 부분(키보드, 마우스, HDD-저장되는 파일)
+		 출력 스트림 : 프로그램으로 나가는 부분(모니터, HDD, 프린터)
+		 Stream : API 라이브러리를 사용하는 형태
+		 JAVA.io (API)
+		 
+		 (일반 : File, FileReader) --> StreamAPI (FileinpuStream)
+		 (일반 : FileWriter) --> StreamAPI(FileoutputStream)
+		*/
+	public static void main(String[] args) throws IOException {
+		FileReader fr = new FileReader("D:\\git_java\\javaproject\\java2\\src\\Files\\message.txt");
+		System.out.println(fr);
 
 
 	}
