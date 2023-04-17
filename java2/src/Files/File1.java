7e473277abd9639fb760b0f3750b358fb92697d9
s0415 <sheepmeow1107@gmail.com>
2023-04-17 14:57:57 +0900

    master

--- java2/src/Files/File1.java
+++ java2/src/Files/File1.java
@@ -14,6 +14,13 @@ public class File1 {
 		 (일반 : File, FileReader) --> StreamAPI (FileinpuStream)
 		 (일반 : FileWriter) --> StreamAPI(FileoutputStream)
 		*/
+	
+	/*
+	 FileReader : 문자 데이터를 파일로부터 읽어들이는 클래스
+	 			  파일 이름을 생성자 인수값(파라미터)로 사용해서 FileReader객체를 생성합니다.
+	 			  
+	 I/O는 throws를 사용하거나, try-catch를 사용해야만 적용할 수 있음.
+	 */
 	public static void main(String[] args) throws IOException {
 		FileReader fr = new FileReader("D:\\git_java\\javaproject\\java2\\src\\Files\\message.txt");
 		while(true) {
---
+++ java2/src/Files/File2.java
@@ -0,0 +1,20 @@
+package Files;
+import java.io.FileReader;
+
+public class File2 {
+
+	public static void main(String[] args) throws Exception {
+		try {
+		FileReader fr = new FileReader("D:\\git_java\\javaproject\\java2\\src\\Files\\message.txt");
+		System.out.println(fr.getEncoding());	//파일 인코딩 언어 확인
+		System.out.println(fr.read());	//데이터 크기
+		System.out.println(fr.read());
+		fr.close();//로드한 파일을 닫을때 사용
+		}
+		catch (Exception e){
+			System.out.println("파일 경로 실패");
+		}
+		
+	}
+
+}
