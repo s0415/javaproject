f5a41ad8b4d0447ab319a1fd578c2e2b37baa815
s0415 <sheepmeow1107@gmail.com>
2023-04-18 16:59:53 +0900

    master

--- java2/src/Files/File16.java
+++ java2/src/Files/File16.java
@@ -11,21 +11,27 @@ public class File16 {
 	
 	public static void main(String[] args) {
 		try {
+			//사용자가 업로드 하는 이미지
 			InputStream is = new FileInputStream("D:\\memo\\asdf.jpg");
-			//FileInputStream is = new FileInputStream("D:\\memo\\asdf.jpg");
-			//byte img[]=new byte[is.available()];
-			byte img[]=new byte[is.available()/100];
-			OutputStream os = new FileOutputStream("asdf2.jpg");
+			//FileInputStream is = new FileInputStream("D:\\memo\\asdf.jpg");	//위와같음
 			
-			int imgs =0;
-			int check=0;
+			
+			//사용자가 이미지 크기를 계산 후 파일을 읽어들이는 크기(단위:byte)
+			
+			//byte img[]=new byte[is.available()];	//메모리 크기만큼 한번에 이미지 사이즈를 읽어들임
+			byte img[]=new byte[is.available()/100];	//100으로 나눠서 해당크기만큼 읽어들임
+			OutputStream os = new FileOutputStream("asdf2.jpg");	//copy 경로 및 파일명
+			
+			int imgs =0;	//읽는 바이트 수
+			int check=0;	//%로 사용자에게 생성률을 보여주기 위한 변수(학습용)
 			while(true) {
-				imgs = is.read();
+				imgs = is.read(img);	//해당 파일을 읽어서 바이트 변수로 넘김
+				System.out.println(imgs);
 				if(imgs ==-1) {
 					break;
 				}
 				else {
-					os.write(img,0,imgs);
+					os.write(img,0,imgs);	//이미지를 copy함
 				}
 				check++;
 				if(check%2 ==0) {
