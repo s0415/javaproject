b033cab615da3eb2fccb87a0ce65b99a5e281da2
s0415 <sheepmeow1107@gmail.com>
2023-04-18 15:38:08 +0900

    master

---
+++ java2/src/Files/File14.java
@@ -0,0 +1,61 @@
+package Files;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class File14 {
+	/*
+	 1. InputStream : FileInputStream(파일 경로) - 불러오기
+	 2. byte[] 배열 크기 선언
+	 3. read로 파일을 읽어옴
+	 4. 출력
+	*/
+	
+	/*
+	  1. OutputStream : FileOutputStream(파일저장) - 저장하기 
+	  2. byte[] 배열 변환(getBytes())
+	  3. write
+	  4. flush
+	 */
+	
+	public static void main(String[] args) {
+		try {
+			loadfile ld = new loadfile();
+			ld.code();
+		}
+		catch(Exception e){
+			
+		}
+	}
+
+}
+
+class loadfile {
+	File f = null;
+	InputStream is = null;	//(int)
+	OutputStream os = null;
+	public void code() throws Exception {
+		//신규로 문자를 적용할 경우 new byte를 새로 계산하여 적용해야 함
+		this.f = new File("D:\\git_java\\javaproject\\java2\\src\\Files\\abc.txt");
+		this.is = System.in;	//사용자가 입력하는 문구
+		byte[] word = new byte[100];	//저장기록 용량
+		this.is.read(word);	//사용자가 입력한 내용을 배열로 저장
+		this.os = new FileOutputStream(f,true);	//입력된 사항 추가 저장
+		
+		//System.out.println(Arrays.toString(word));
+		//this.os = new FileOutputStream(f,true);
+
+		this.os.write(word);	//파일에 입력 문자 저장
+		this.os.flush();		//메모리 초기화
+		this.os.close();		//파일 닫기
+		this.is.close();
+		
+		//int word;
+		//word=this.is.read();
+		//System.out.println(word);
+	
+	}
+		
+}
