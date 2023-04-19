833b9d1c6eca33629c4bbeb14ce05d1bd5e0ca15
s0415 <sheepmeow1107@gmail.com>
2023-04-19 15:46:27 +0900

    master

--- java2/src/Network/Net5.java
+++ java2/src/Network/Net5.java
@@ -1,5 +1,7 @@
 package Network;
 
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -16,8 +18,36 @@ public class Net5 {
 			ServerSocket ss= new ServerSocket(port);	//오픈시킬 포트 적용
 			while(true) {
 				System.out.println("연결 대기중 ... ");
-				Socket sc = ss.accept();	//accept : 클라이언트가 들어오는것을 대기하는 역할? ?
+				
+				//accept : 클라이언트가 들어오는것을 대기하는 역할? ?
+				Socket sc = ss.accept();	//대기
+				
+				//클라이언트가 접속했을 경우 해당 라인을 시작합니다.
+				//접속완료
 				System.out.println("호스트 : 통신 연결 성공");
+				
+				//서버에서 클라이언트로 받는 통로
+				InputStream is = sc.getInputStream();
+				
+				//클라이언트로 보내는 통로
+				OutputStream os = sc.getOutputStream();
+				
+				//클라이언트에서 받는 텍스트를 byte로 받음
+				byte data[] = new byte[1024];
+				int n = is.read(data);	//해당값을 읽어들임
+				
+				String msgclient = new String(data,0,n);//문자자료형 변환
+				System.out.println(msgclient);	//출력
+				
+				//클라이언트로 메세지전송
+				String aws ="반갑습니다.뿡";	//서버에서 보내는메세지
+				os.write(aws.getBytes());	//클라이언트로 보내기위한 메모리저장
+				
+				os.flush();
+				os.close();
+				is.close();
+				sc.close();
+				
 			}
 		}
 		catch(Exception e){
--- java2/src/Network/Net6.java
+++ java2/src/Network/Net6.java
@@ -1,5 +1,7 @@
 package Network;
 
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.Socket;
 
 public class Net6 {
@@ -9,10 +11,27 @@ public class Net6 {
 	public static void main(String[] args) {
 		try {
 			System.out.println("Client 접속");
-			String ip = "192.168.10.143";
-			int port =9001;
+			String ip = "192.168.10.144";
+			int port =9001;	
 			Socket sk = new Socket(ip,port);
-			System.out.println("서버에 연결 되었습니다.");
+			System.out.println("서버에 연결 되었습니다. 클라이언트1");
+			
+			//클라이언트에서 서버로 보내는 메세지
+			String cmsg = "안녕하세요";
+			
+			//클라이언트에서 서버로 보내는 통로
+			OutputStream os = sk.getOutputStream();
+			os.write(cmsg.getBytes());
+			os.flush();
+			
+			//서버에서 보낸 메세지를 클라이언트가 받는 통로
+			byte[] data = new byte[1024];
+			InputStream is = sk.getInputStream();
+			int n = is.read(data);
+			String sm = new String(data,0,n);
+			System.out.println(sm);
+			sk.close();
+			os.close();
 			
 		}
 		catch(Exception e) {
