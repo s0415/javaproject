3e12d1fb8499d8c8fc5ccffad24cb3cfcac8a2fc
s0415 <sheepmeow1107@gmail.com>
2023-04-19 13:21:51 +0900

    master

--- java2/src/Network/Net3.java
+++ java2/src/Network/Net3.java
@@ -10,6 +10,7 @@ import java.net.URLConnection;
 import java.util.Scanner;
 
 //Crawling 사용법
+//PrintWriter 사용하여 출력
 public class Net3 {
 
 	public static void main(String[] args) {
@@ -30,25 +31,33 @@ class crawling{
 	URL urls = null;
 	InputStream is = null;
 	InputStreamReader ir = null;
+	
 	public void creak() throws Exception{
 		this.sc = new Scanner(System.in);
 		System.out.println("크롤링 할 URL 주소를 입력하세요(파일명 포함)");
 		this.url = this.sc.nextLine();
 		this.urls = new URL(this.url);
 		URLConnection con = this.urls.openConnection();
-		this.is = this.urls.openStream();
-		this.ir=new InputStreamReader(this.is);
-		BufferedReader br = new BufferedReader(this.ir);
 		
-		FileOutputStream fs = new FileOutputStream("E:\\memo\\test.html");
-		PrintWriter pw = new PrintWriter(fs);
-		String code = "";
+		this.is = this.urls.openStream();	//통신에는 openStream만 사용, 그 외의 Stream 사용X
+		
+		this.ir=new InputStreamReader(this.is);	//url 소스파일 읽기
+		BufferedReader br = new BufferedReader(this.ir);	//읽은 소스파일을 메모리에 탑재(저장
+		//파일명생성
+		FileOutputStream fs = new FileOutputStream("D:\\memo\\test.html",true);	
+
+		
+		//*PrintWriter 사용하지 않고 출력 : Net4
+		PrintWriter pw = new PrintWriter(fs);	//write에 String사용 위해서 출력을 문자화하여 사용
+		String code = "";	
 		while((code=br.readLine())!=null) {
-			pw.write(code);
+			pw.write(code);	//write에 String으로 사용
 		}
+		
 		fs.flush();
 		fs.close();
 		this.sc.close();
 		pw.close();
+		System.out.println("크롤링이 모두 완료되었습니다.");
 	}
 }
