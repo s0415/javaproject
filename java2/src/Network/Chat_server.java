03bb67ec03e1a21371f9b853109706c6bcfbfac4
s0415 <sheepmeow1107@gmail.com>
2023-04-20 13:00:54 +0900

    master

--- java2/src/Network/Chat_server.java
+++ java2/src/Network/Chat_server.java
@@ -10,7 +10,8 @@ public class Chat_server {
 	//채팅서버
 	public static void main(String[] args) {
 		
-
+		chat_sv cs = new chat_sv();
+		cs.chatserver();
 	}
 
 }
@@ -21,5 +22,23 @@ class chat_sv{
 	Scanner sc = null;
 	InputStream is = null;
 	OutputStream os = null;
-	
+	private String mid = null;
+	public void chatserver() {
+		try {
+			this.sk=new ServerSocket(this.port);
+			this.sc=new Scanner(System.in);
+			System.out.println("Login ID : ");	//채팅방 개설자 아이디
+			this.mid = sc.next();
+			System.out.println("Chatting Room Open");
+			while(true) {
+				this.so = sk.accept();
+				this.is = this.so.getInputStream();		//클라이언트에게 받는 메세지
+				this.os = this.so.getOutputStream();	//클라이언트에게 보내는 메세지
+			}
+		}
+		catch(Exception e) {
+			System.out.println("Port Connect Error!");
+		}
+		
+	}
 }
